2588f7c6d755f868297c5bcbc627d0122c3aa2a0
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-02-24 09:44:31 -0800

    commit tree ad6f2b160bdda3164188a40ef90a71f290c36694

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -6,16 +6,28 @@ class Person
   end
 
   def get_married(person)
-    self.partner = person
-    person.partner = self
+    if person.is_a?(Person)
+      self.partner = person
+      person.partner = self
+    else
+      begin
+        raise PatnerError
+      rescue PartnerError => error
+        puts error.message
+      end
+    end
   end
 
+  class PatnerError < StandardError
+    def message
+      "do something based on this error message"
+    end
+  end
 end
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
 
 
 
